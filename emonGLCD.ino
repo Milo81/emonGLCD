5a45884f0eb5189710567246b52ac8571ef3471f
Miloslav Fritz <me@miloslavfritz.com>
2014-02-11 20:10:10 +0100

    Better implementation of icing conditions. Changed after reading http://www.atmos.washington.edu/cliff/Roadway3.html

--- emonGLCD.ino
+++ emonGLCD.ino
@@ -218,10 +218,10 @@ void loop()
       GREEN = 0;
     }
 
-    // special feature - in morning when i wake up, and it's around zero
-    // degrees outside, blink red LED, so I know that I will have to
-    // deice windshield on my car probably...
-    if ((hour > 5 || hour < 7) && emonth2.temp1 < 10) red_blink = 1;
+    // special feature - in morning when i wake up, and both dew point
+    // and temperature are both below zero, blink red LED, so I know that I will have to
+    // deice windshield on my car (probably)...
+    if ((hour > 5 || hour < 7) && emonth2.temp1 < 10 && dew_point2 < 0) red_blink = 1;
     else red_blink = 0;
     
   
